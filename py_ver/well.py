9f841ed1f81ebfd9ec1f6b28d084912812d3ca43
Arkavo Hait <arkavohait@gmail.com>
2021-09-15 21:20:42 +0530

    edits

--- py_ver/well.py
+++ py_ver/well.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-L = 200
+L = 500
 dx = 0.2
+step = -0.2
 Vx = np.zeros(L)
-Vx[0] = 10
-Vx[-1]= 10
+Vx[0] = 100
+Vx[-1]= 100
 #plt.plot(Vx)
 
 """
@@ -14,11 +15,11 @@ Hy = Ey
 => ay'' + Ey = 0
 """
 Y = np.ones_like(Vx,dtype=float)
-E = -4
+E = -6
 dY = np.zeros_like(Vx,dtype=float)
 ddY = np.zeros_like(Vx,dtype=float)
 
-Tol = 5000
+Tol = 10000
 Max = 10
 
 tmp_Y = np.zeros_like(Vx,dtype=float)
@@ -31,9 +32,9 @@ while(Tol>0):
     #    Y[i] = tmp_Y[i]
     for i in range(L):
         if i>0 and i<L-1:
-            ddY[i] = (Vx[i] - E - (Y[i+1]-Y[i-1])/2.)*-0.05
+            ddY[i] = (Vx[i] - E - (Y[i+1]-Y[i-1])/2.)*step
         else:
-            ddY[i] = (Vx[i] - E)*-0.05
+            ddY[i] = (Vx[i] - E)*step
     for i in range(L):
         if i>0 and i<L-1:
             dY[i] += (ddY[i-1]+ddY[i+1])*dx/2
@@ -43,17 +44,21 @@ while(Tol>0):
         Y[i] += dY[i]*dx
         #if dY[i]*dx < Tol:
         #    break
-    print(str(Tol)+"/5000",end='\r')
+    print(str(Tol)+"/10000",end='\r')
     #for i in range(L):
     #    if(Max < np.abs(Y[i]-tmp_Y[i])):
     #        Max = np.abs(Y[i]-tmp_Y[i])
     #        print(Max)
 sum = 0
+prob = []
 for i in range(L):
     sum += Y[i]**2
+    prob.append(Y[i]**2)
 sum = sum**(0.5)
 Y = Y/(sum)
 print("A = "+str(float(sum)),end='\n')
 plt.plot(Y)
+prob = np.array(prob)
+plt.plot(prob/float(sum**2))
 #plt.plot(ddY)
 plt.savefig('well.jpg')
--- src/test.cu
+++ src/test.cu
@@ -46,19 +46,27 @@ __global__ void SETUP(double* dest, double val)
     *(dest+idx) = val;
 }
 
-__global__ void DERIVATIVE_1(double* y, double* ddy, double* V, double* E,double step, int L)
+__global__ void DERIVATIVE_STEP(double* y, double* ddy, double* V, double* E,double step, int L)
 {
-    idx = threadIdx.x + blockIdx.x*blockDim.x;
+    int idx = threadIdx.x + blockIdx.x*blockDim.x;
+    if((idx>0) && (idx<(L-1)))
     {
-        if idx>0 && idx<(L-1)
-        *(dy+idx) = (*(y+1+idx)-*(y-1+idx))/(2*step);
+        *(ddy+idx) = (*(y+1+idx)-*(y-1+idx))/(2* (step)) + *(V+idx) - (*E);
+    }
+    else
+    {
+        *(ddy+idx) = (*(V+idx) - *E) * (step);
     }
 }
+
 
 
 int main(int argc,char* argv[])
 {
     int L = stoi(argv[1]);    
+    L = 10;
+    int blocks = 2;
+    int threads = 5;
     std::cout<<"Length: "<<L<<"\n";
     particle test;
     test.mass = 0;
@@ -67,33 +75,42 @@ int main(int argc,char* argv[])
     unsigned long long SIZE_0 = ((int)sizeof(double)*L);
     double* V_HST;
     double* V_DEV;
-    
-    
+    double Ev = 3.0;
+    double* E;
+    double step = 0.2;
     double* Y_DEV;
     double* dY_DEV;
     double* ddY_DEV;
     double* Y_Final;
     //setup
-    int loops = 100;
+    int loops = 0;
     V_HST = (double*)malloc(SIZE_0);
     Y_Final = (double*)malloc(SIZE_0);
+    
     cudaMalloc((void**)&V_DEV,SIZE_0);
     cudaMalloc((void**)&Y_DEV,SIZE_0);
     cudaMalloc((void**)&dY_DEV,SIZE_0);
     cudaMalloc((void**)&ddY_DEV,SIZE_0);
+    cudaMalloc((void**)&E,sizeof(double));
     
-    set_energy(V_HST,L,2.2);
+    set_energy(V_HST,L,2.3);
     cudaMemcpy(V_DEV,V_HST,SIZE_0,cudaMemcpyHostToDevice);
-    SETUP <<<4,4>>> (Y_DEV,0);
-    SETUP <<<4,4>>> (Y_DEV,0);
-    SETUP <<<4,4>>> (Y_DEV,0);
-    SETUP <<<4,4>>> (Y_DEV,0);
+    cudaMemcpy(E,&Ev,int(sizeof(double)),cudaMemcpyHostToDevice);
+    
+    SETUP <<<blocks,threads>>> (Y_Final,0.);
+    SETUP <<<blocks,threads>>> (V_DEV,0.); 
+    SETUP <<<blocks,threads>>> (Y_DEV,0.);
+    SETUP <<<blocks,threads>>> (dY_DEV,0.);
+    SETUP <<<blocks,threads>>> (ddY_DEV,0.);
     
     while(loops>0)
     {
-
+        loops-=1;
+        std::cout<<loops<<"\n";
+        DERIVATIVE_STEP <<<blocks,threads>>> (ddY_DEV,Y_DEV,V_DEV,E,step,L);
     }
-
-    display(V_HST,L);
+    cudaMemcpy(Y_Final,ddY_DEV,SIZE_0,cudaMemcpyDeviceToHost);
+    std::cout<<"\n";
+    display(Y_Final,L);
     return 0;
 }
