1cb339262f7130ac55d2172e78f6c97d8f741d59
Arkavo Hait <arkavohait@gmail.com>
2021-09-22 20:53:05 +0530

    modulation

--- cuQMstd.cuh
+++ cuQMstd.cuh
@@ -10,7 +10,7 @@ struct vector3
 
 
 
-__global__ void ALLOCATE(double* ADDRESS,double *DATA)
+__global__ void ASSIGN(double* ADDRESS,double *DATA)
 {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
     *(ADDRESS + idx) = *(DATA + idx);
@@ -19,9 +19,30 @@ __global__ void ALLOCATE(double* ADDRESS,double *DATA)
 class SPACE
 {
     public:
-        double ADDRESS;
+        double* ADDRESS;
+        double* V;
+        double* Y;
+        double* dY;
+        double* ddY;
+
         int SIZE_X;
         int SIZE_Y;
         int SIZE_Z;
+
+    void initialize()
+    {
+        unsigned long long SIZE0 = int(SIZE_X*SIZE_Y*SIZE_Z*sizeof(double));
+        
+        cudaMalloc((void**)&ADDRESS,SIZE0);
+        cudaMalloc((void**)&V,SIZE0);
+        cudaMalloc((void**)&Y,SIZE0);
+        cudaMalloc((void**)&dY,SIZE0);
+        cudaMalloc((void**)&ddY,SIZE0);
+    }
+    
+    void assign(double* DATA)
+    {
+        ASSIGN <<<4,int(SIZE_X/4)>>> (ADDRESS,DATA);
+    }
         
 };
--- sample.cu
+++ sample.cu
@@ -1,6 +1,20 @@
 #include "cuQMstd.cuh"
+#include <iostream>
 
 int main()
 {
+    SPACE sample1;
+    sample1.SIZE_X = 10;
+    sample1.SIZE_Y = 10;
+    sample1.SIZE_Z = 10;
+    
+    sample1.initialize();
+    
+    std::cout<<sample1.ADDRESS<<'\n'
+    <<sample1.V<<'\n'
+    <<sample1.Y<<'\n'
+    <<sample1.dY<<'\n'
+    <<sample1.ddY<<'\n';   
+    
     return 0;
 }
