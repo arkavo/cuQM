327917d0d0e620547f453d5ce9066b1baa032a1c
Arkavo Hait <arkavohait@gmail.com>
2021-09-22 21:14:10 +0530

    modulation

--- cuQMstd.cuh
+++ cuQMstd.cuh
@@ -24,25 +24,50 @@ class SPACE
         double* Y;
         double* dY;
         double* ddY;
+        double* host_debug;
 
         int SIZE_X;
         int SIZE_Y;
         int SIZE_Z;
 
+    unsigned long long SIZE0 = int(SIZE_X*SIZE_Y*SIZE_Z*sizeof(double));
+    
     void initialize()
     {
-        unsigned long long SIZE0 = int(SIZE_X*SIZE_Y*SIZE_Z*sizeof(double));
+        
         
         cudaMalloc((void**)&ADDRESS,SIZE0);
         cudaMalloc((void**)&V,SIZE0);
         cudaMalloc((void**)&Y,SIZE0);
         cudaMalloc((void**)&dY,SIZE0);
         cudaMalloc((void**)&ddY,SIZE0);
+        host_debug = (double*)malloc(SIZE0);
     }
     
     void assign(double* DATA)
     {
-        ASSIGN <<<4,int(SIZE_X/4)>>> (ADDRESS,DATA);
+        cudaMemcpy(ADDRESS,DATA,SIZE0,cudaMemcpyHostToDevice);
+    }
+
+    void display()
+    {
+        cudaMemcpy(host_debug,ADDRESS,SIZE0,cudaMemcpyDeviceToHost);
+        for(int i=0;i<SIZE_X;i++)
+        {
+            for(int j=0;j<SIZE_Y;j++)
+            {
+                for(int k=0;k<SIZE_Z;k++)
+                {
+                    std::cout<<*(host_debug+i+j+k)<<' ';
+                }
+                std::cout<<'\n';
+            }
+            for(int i=0;i<SIZE_X;i++)
+            {
+                std::cout<<"_";
+            }
+            std::cout<<"\n";
+        }
     }
         
 };
--- sample.cu
+++ sample.cu
@@ -5,16 +5,31 @@ int main()
 {
     SPACE sample1;
     sample1.SIZE_X = 10;
-    sample1.SIZE_Y = 10;
-    sample1.SIZE_Z = 10;
+    sample1.SIZE_Y = 1;
+    sample1.SIZE_Z = 1;
     
     sample1.initialize();
+    double* space_arr;
+    space_arr = (double*)malloc(10*sizeof(double));
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            for(int k=0;k<10;k++)
+            {
+                *(space_arr+i+j+k) = i+j+k -i*j*k;
+                //std::cout<<space_arr[i][j][k]<<'\n';
+            }
+        }
+    }
     
-    std::cout<<sample1.ADDRESS<<'\n'
+    /*std::cout<<sample1.ADDRESS<<'\n'
     <<sample1.V<<'\n'
     <<sample1.Y<<'\n'
     <<sample1.dY<<'\n'
     <<sample1.ddY<<'\n';   
-    
+    */
+    sample1.assign(space_arr);
+    sample1.display();
     return 0;
 }
